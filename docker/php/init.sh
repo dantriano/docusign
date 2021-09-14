f217c76f4f2704f200903091be1aff662e32d338
Dan <dan.triano@gmail.com>
2021-09-15 00:00:18 +0200

    Compose for prod

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -47,19 +47,19 @@ apt-get update && apt-get install -y \
 #apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Install extensions
-#docker-php-ext-install pdo_mysql zip exif pcntl
-#docker-php-ext-configure gd --with-freetype --with-jpeg
+docker-php-ext-install pdo_mysql zip exif pcntl
+docker-php-ext-configure gd --with-freetype --with-jpeg
 
 #RUN docker-php-ext-configure gd --enable-gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ --with-png-dir=/usr/include/
-#docker-php-ext-install gd
+docker-php-ext-install gd
 
 # Install composer
-#curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 #Uncomment permission fix and resolvers in case to be unable to resolve Debian repositories:
 
 #chmod o+r /etc/resolv.conf
 #echo "nameserver 8.8.8.8" >> /etc/resolv.conf 
 #npm run dev
-#composer install
+composer install
 php-fpm
